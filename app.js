9f11a39f25fe34e42bcfdcb0f2f24a1c73e27790
ezservices <ezservices1920@gmail.com>
2020-04-22 00:25:30 +0530

    MVC structure created

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const ejs = require("ejs")
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 
-
 const app = express();
+
+const indexRouter = require("./routes/index");
+const loginRouter = require("./routes/login");
+const signupRouter = require("./routes/signup");
+
+
+mongoose.connect('mongodb://localhost:27017/servicesDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/",(req,res) => {
-    res.render("index")
-})
+app.use('/',indexRouter);
+app.use('/login',loginRouter);
+app.use('/signup',signupRouter);
 
 
 app.listen(3000, function() {
